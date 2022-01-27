0d3ee33ab28c165ed85aba0e32999413e41f2f7f
Bill Mårtensson <bill@magictechnology.se>
2022-01-27 10:07:55 +0100

    piax

---
+++ app/src/main/java/se/magictechnology/piaxnav/StartFragment.kt
@@ -0,0 +1,32 @@
+package se.magictechnology.piaxnav
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+class StartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.goReadmoreButton).setOnClickListener {
+
+            val action = StartFragmentDirections.actionStartFragmentToReadmoreFragment()
+
+            view.findNavController().navigate(action)
+
+        }
+    }
+}
